42c41c4f4f27a30b62bafc3b8c6634f7919bd537
mahmoud126d <mahmouud.salah2@gmail.com>
2025-06-03 15:03:11 +0300

    Initial Commit

---
+++ Shoplet-Admin/Shoplet_AdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shoplet_AdminApp.swift
+//  Shoplet-Admin
+//
+//  Created by Macos on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Shoplet_AdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
